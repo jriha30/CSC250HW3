3d3b4aeeb41764a410832c9f7ff4c53db09a6fda
Jason Riha <superjar30@gmail.com>
2020-09-11 18:11:48 -0500

    added a new function to convert from decimal to any other base below base 10.

--- src/Driver.java
+++ src/Driver.java
@@ -6,8 +6,7 @@ public class Driver
 	{
 		decToBin();
 	}
-	
-	static String decToBin()
+	static void decToBin()
 	{
 		Scanner myObj = new Scanner(System.in);
 		System.out.print("Please enter a decimal number: ");
@@ -21,6 +20,23 @@ public class Driver
 			binString = remainder + binString;
 		}
 		System.out.println("The decimal number " + decString + " is " + binString + " in binary.");
-		return binString;
+	}
+	static void decToAnyBase()
+	{
+		Scanner myObj = new Scanner(System.in);
+		System.out.print("Please enter a decimal number: ");
+		String decString = myObj.nextLine();
+		System.out.print("What number base would you like to convert to?: ");
+		String baseConversionString = myObj.nextLine();
+		int baseConversion = Integer.parseInt(baseConversionString);
+		int decNum = Integer.parseInt(decString);
+		String binString = "";
+		while(decNum != 0)
+		{
+			int remainder = decNum % baseConversion;
+			decNum = decNum / baseConversion;
+			binString = remainder + binString;
+		}
+		System.out.println("The decimal number " + decString + " is the number " + binString + " in base " + baseConversionString + ".");
 	}
 }
