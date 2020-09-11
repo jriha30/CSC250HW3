5626ba19c28b899391c12eb0b2bd5931a1fd92b7
Jason Riha <superjar30@gmail.com>
2020-09-11 10:31:58 -0500

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Driver
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		Scanner myObj = new Scanner(System.in);
+		String decString = myObj.nextLine();
+		int decNum = Integer.parseInt(decString);
+		String binString = "";
+		while(decNum != 0)
+		{
+			int remainder = decNum % 2;
+			decNum = decNum / 2;
+			binString = remainder + binString;
+		}
+		System.out.println("The decimal number " + decString + " is " + binString + " in binary.");
 	}
 }
